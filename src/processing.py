07c74579e6ee88bd4c7cda47dc7eba21f320ab91
eweilow <eweilow@digitalkoppar.se>
2020-11-23 20:42:14 +0100

    Tweak processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,6 @@
 import time
 import io
+import math
 import datetime
 import dateutil.parser
 from influxdb import InfluxDBClient
@@ -10,7 +11,7 @@ client = InfluxDBClient('localhost', 8086, 'waterrig', 'waterrigpw', 'watertestr
 start = time.time()
 while True:
   print("Fetching")
-  result = client.query('SELECT mean(voltage0) as voltage0, mean(voltage1) as voltage1, mean(instantaneous) as instantaneous, mean(total) as total FROM adc, flow WHERE time >= now() - 5m GROUP BY time(1000ms) fill(null);')
+  result = client.query('SELECT mean(voltage0) as voltage0, mean(voltage1) as voltage1, mean(instantaneous) as instantaneous, mean(total) as total FROM adc, flow WHERE time >= now() - 60m GROUP BY time(1s) fill(null);')
   
   points = list(result.get_points(measurement="adc"))
   measurementPoints = list(result.get_points(measurement="flow"))
@@ -28,13 +29,16 @@ while True:
   transducerVoltage = np.array([point["voltage0"] for point in points])
   voltageDrop = np.array([(5.0 - point["voltage1"]) / 2 if not point["voltage1"] == None else None for point in points])
 
-  ratio = np.array([(transducerVoltage[i]) / 5.0 if not transducerVoltage[i] == None else None for i in range(len(points))])
-  pressure = np.array([1378951.46*((val - 0.1) / 0.8) / 1e5 if not val == None else None for val in ratio])
+  baseLevel = 0.5125
+  ratio = np.array([(transducerVoltage[i] - baseLevel) / 4.0 if not transducerVoltage[i] == None else None for i in range(len(points))])
 
-  instantaneous = np.array([point["instantaneous"] for point in measurementPoints])
-  total = np.array([point["total"] for point in measurementPoints])
+  maxPressure = 1.2e6 # 1378951.46
+  pressure = np.array([maxPressure*val / 1e5 if not val == None else None for val in ratio])
 
-  fig = plt.figure(figsize=(12, 6))
+  instantaneous = np.array([point["instantaneous"] if not point["instantaneous"] == None else None for point in measurementPoints])
+  total = np.array([point["total"] if not point["total"] == None else None for point in measurementPoints])
+
+  fig = plt.figure(figsize=(16, 8))
 
   print("Drawing")
 
@@ -74,11 +78,30 @@ while True:
   plt.ylabel("Total flow (l)")
   plt.grid()
 
+  waterDensity = 997
+
+  massFlow = np.array([point / 1000 * waterDensity if not point == None else None for point in instantaneous])
+
   plt.subplot(2,3,6)
-  plt.plot(pressure, instantaneous)
-  plt.ylabel("Flow (l/s)")
+  plt.plot(pressure, massFlow, '.')
+  plt.ylabel("Flow (kg/s)")
   plt.xlabel("Pressure (bar)")
   plt.grid()
+
+  pressureWithoutNone = [point if not point == None else 0 for point in pressure]
+  pressures = np.linspace(0, np.max(pressureWithoutNone), 50)
+  legends = ("Measured",)
+  for cD in [0.25, 0.5, 0.75, 1]:
+    numHoles = 13
+    holeDiameterMm = 1.5
+    area = numHoles * pow((holeDiameterMm/1000.0)/2.0, 2.0) * math.pi
+
+    theoretical = cD * area * np.sqrt(2 * waterDensity * pressures * 1e5)
+
+    plt.plot(pressures, theoretical, '--')
+    legends = (*legends, "Cd = {:.2f}".format(cD))
+  plt.legend(legends)
+    
 
   plt.suptitle("{:.2f} seconds since startup".format(uptime))
   plt.tight_layout(pad=1.1, rect=(0, 0, 1, 0.95))
