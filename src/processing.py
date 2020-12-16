d799d62668e06d343fc975a8650c88c0e8201132
eweilow <eweilow@digitalkoppar.se>
2020-12-16 21:43:02 +0100

    Just dump data, don't plot

--- src/processing.py
+++ src/processing.py
@@ -1,28 +1,57 @@
 import time
-import io
+import os
 import math
 import datetime
 import dateutil.parser
+
 from influxdb import InfluxDBClient
-import matplotlib.pyplot as plt
-import numpy as np
+
 client = InfluxDBClient('localhost', 8086, 'waterrig', 'waterrigpw', 'watertestrig')
 
 start = time.time()
 while True:
-  print("Fetching")
-  result = client.query('SELECT mean(voltage0) as voltage0, mean(voltage1) as voltage1, mean(instantaneous) as instantaneous, mean(total) as total FROM adc, flow WHERE time >= now() - 60m GROUP BY time(1s) fill(null);')
-  
-  points = list(result.get_points(measurement="adc"))
-  measurementPoints = list(result.get_points(measurement="flow"))
+  for ranges in [("60m", "1s")]:
+    fullRange, interval = ranges
+    
+    filename = '/var/www/html/data_{:}.txt'.format(fullRange)
+    tmp_filename = '{:}.tmp'.format(filename)
 
-  if len(points) == 0:
-    time.sleep(5)
-    pass
+    print("Fetching {:} with step {:}".format(fullRange, interval))
+    result = client.query('SELECT mean(voltage0) as voltage0, mean(voltage1) as voltage1, mean(instantaneous) as instantaneous, mean(total) as total FROM adc, flow WHERE time >= now() - {:} GROUP BY time({:}) fill(null);'.format(fullRange, interval))
+    
+    points = list(result.get_points(measurement="adc"))
+    measurementPoints = list(result.get_points(measurement="flow"))
 
-  print("found {:} points".format(len(points)))
+    if len(points) == 0:
+      time.sleep(5)
+      continue
+
+    print("found {:} points".format(len(points)))
+
+    import csv
+    with open(tmp_filename, 'w', newline='') as csvfile:
+      writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    
+      lastTime = dateutil.parser.isoparse(points[len(points) - 1]["time"]).timestamp()
+      for i in range(len(points)):
+        currentTime = dateutil.parser.isoparse(points[i]["time"]).timestamp() - lastTime
+        voltage0 = points[i]["voltage0"]
+        voltage1 = points[i]["voltage1"]
+
+        instantaneous = measurementPoints[i]["instantaneous"]
+        total = measurementPoints[i]["total"]
+
+        writer.writerow([
+          currentTime, voltage0, voltage1, instantaneous, total
+        ])
+
+    os.rename(tmp_filename, filename)
+    
+
+  continue # Don't plot?
 
   print("Processing")
+  import numpy as np
   times = np.array([dateutil.parser.isoparse(point["time"]).timestamp() for point in points])
   times = times - times[-1]
   
